a2e3f10c28f5946eac2aaa356f14cc99c47d262b
marco <marcomurro9@gmail.com>
2024-02-06 16:31:50 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Rakendus küsib kasutaja sugu (m/f) ja perekonnanime
+// Lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+// "Welcome, Mr. [kasutaja perekonnanimi]" / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine()); // Loeb konsoolist maha andmeid sõne formaadis
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
---
+++ Week2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, world!");
+// Uued andmetüübid: int ja char
+// if-statements
+
